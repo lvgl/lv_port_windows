68fbab4471830cdda7bae9b2b2e9fd1d319b29d1
MouriNaruto <Mouri_Naruto@outlook.com>
2021-06-20 19:28:47 +0800

    Improve the implementation of Windows GDI font backend.

--- LVGL.Windows/LVGL.Windows.Font.cpp
+++ LVGL.Windows/LVGL.Windows.Font.cpp
@@ -15,6 +15,7 @@
 
 #include <cstdint>
 #include <map>
+#include <vector>
 
 static FIXED ConvertDoubleToFixed(double value)
 {
@@ -28,10 +29,11 @@ typedef struct _LVGL_WINDOWS_GDI_FONT_CONTEXT
 {
     HFONT FontHandle;
     HFONT SymbolFontHandle;
-    OUTLINETEXTMETRICW OutlineTextMetrics;
-    std::map<std::uint32_t, std::pair<GLYPHMETRICS, std::uint8_t*>> GlyphSet;
+    using GlyphValueType = std::pair<lv_font_glyph_dsc_t, std::size_t>;
+    std::map<std::uint32_t, GlyphValueType> GlyphSet;
     std::uint32_t DpiValue;
-} LVGL_WINDOWS_GDI_FONT_CONTEXT, * PLVGL_WINDOWS_GDI_FONT_CONTEXT;
+    std::vector<std::uint8_t> GlyphBitmapPool;
+} LVGL_WINDOWS_GDI_FONT_CONTEXT, *PLVGL_WINDOWS_GDI_FONT_CONTEXT;
 
 static void LvglWindowsGdiFontAddGlyph(
     PLVGL_WINDOWS_GDI_FONT_CONTEXT Context,
@@ -88,8 +90,7 @@ static void LvglWindowsGdiFontAddGlyph(
         }
 
         GLYPHMETRICS GlyphMetrics;
-        uint8_t* GlyphBitmap = nullptr;
-
+        std::size_t GlyphBitmapOffset = static_cast<std::size_t>(-1);
         DWORD Length = ::GetGlyphOutlineW(
             ContextDCHandle,
             OutBuffer[0],
@@ -102,32 +103,44 @@ static void LvglWindowsGdiFontAddGlyph(
         {
             if (Length > 0)
             {
-                GlyphBitmap = new uint8_t[Length];
-                if (GlyphBitmap)
+                GlyphBitmapOffset = Context->GlyphBitmapPool.size();
+                Context->GlyphBitmapPool.resize(GlyphBitmapOffset + Length);
+                uint8_t* GlyphBitmap = &Context->GlyphBitmapPool[GlyphBitmapOffset];
+
+                if (::GetGlyphOutlineW(
+                    ContextDCHandle,
+                    OutBuffer[0],
+                    GGO_GRAY8_BITMAP | GGO_GLYPH_INDEX,
+                    &GlyphMetrics,
+                    Length,
+                    GlyphBitmap,
+                    &TransformationMatrix) != GDI_ERROR)
                 {
-                    if (::GetGlyphOutlineW(
-                        ContextDCHandle,
-                        OutBuffer[0],
-                        GGO_GRAY8_BITMAP | GGO_GLYPH_INDEX,
-                        &GlyphMetrics,
-                        Length,
-                        GlyphBitmap,
-                        &TransformationMatrix) != GDI_ERROR)
+                    for (size_t i = 0; i < Length; ++i)
                     {
-                        for (size_t i = 0; i < Length; ++i)
-                        {
-                            GlyphBitmap[i] =
-                                GlyphBitmap[i] == 0x40
-                                ? 0xFF
-                                : GlyphBitmap[i] << 2;
-                        }
+                        GlyphBitmap[i] =
+                            GlyphBitmap[i] == 0x40
+                            ? 0xFF
+                            : GlyphBitmap[i] << 2;
                     }
                 }
             }
 
+            lv_font_glyph_dsc_t GlyphDescription;
+            GlyphDescription.adv_w = GlyphMetrics.gmCellIncX;
+            GlyphDescription.box_w = static_cast<std::uint16_t>(
+                (GlyphMetrics.gmBlackBoxX + 0x0003) & 0xFFFC);
+            GlyphDescription.box_h = static_cast<std::uint16_t>(
+                GlyphMetrics.gmBlackBoxY);
+            GlyphDescription.ofs_x = static_cast<std::int16_t>(
+                GlyphMetrics.gmptGlyphOrigin.x);
+            GlyphDescription.ofs_y = static_cast<std::int16_t>(
+                GlyphMetrics.gmptGlyphOrigin.y - GlyphMetrics.gmBlackBoxY);
+            GlyphDescription.bpp = 8;
+
             Context->GlyphSet.emplace(std::make_pair(
                 UnicodeLetter,
-                std::make_pair(GlyphMetrics, GlyphBitmap)));
+                std::make_pair(GlyphDescription, GlyphBitmapOffset)));
         }
 
     } while (false);
@@ -135,6 +148,24 @@ static void LvglWindowsGdiFontAddGlyph(
     if (ContextDCHandle)
     {
         ::ReleaseDC(nullptr, ContextDCHandle);
+    }
+}
+
+static void LvglWindowsGdiFontCheckDpiValue(
+    PLVGL_WINDOWS_GDI_FONT_CONTEXT Context)
+{
+    std::uint32_t DpiValue = LV_DPI_DEF;
+    lv_disp_t* CurrentDisplay = ::lv_disp_get_default();
+    if (CurrentDisplay)
+    {
+        DpiValue = CurrentDisplay->driver->dpi;
+    }
+
+    if (DpiValue != Context->DpiValue)
+    {
+        Context->GlyphSet.clear();
+        Context->GlyphBitmapPool.clear();
+        Context->DpiValue = DpiValue;
     }
 }
 
@@ -150,18 +181,7 @@ static bool LvglWindowsGdiFontGetGlyphCallback(
         reinterpret_cast<PLVGL_WINDOWS_GDI_FONT_CONTEXT>(
             const_cast<void*>(font->dsc));
 
-    std::uint32_t DpiValue = LV_DPI_DEF;
-    lv_disp_t* CurrentDisplay = ::lv_disp_get_default();
-    if (CurrentDisplay)
-    {
-        DpiValue = CurrentDisplay->driver->dpi;
-    }
-
-    if (DpiValue != Context->DpiValue)
-    {
-        Context->GlyphSet.clear();
-        Context->DpiValue = DpiValue;
-    }
+    ::LvglWindowsGdiFontCheckDpiValue(Context);
 
     auto iterator = Context->GlyphSet.find(unicode_letter);
     if (iterator == Context->GlyphSet.end())
@@ -177,18 +197,10 @@ static bool LvglWindowsGdiFontGetGlyphCallback(
         }
     }
 
-    GLYPHMETRICS& GlyphMetrics = iterator->second.first;
-
-    dsc_out->adv_w = GlyphMetrics.gmCellIncX;
-    dsc_out->box_w = static_cast<std::uint16_t>(
-        (GlyphMetrics.gmBlackBoxX + 0x0003) & 0xFFFC);
-    dsc_out->box_h = static_cast<std::uint16_t>(
-        GlyphMetrics.gmBlackBoxY);
-    dsc_out->ofs_x = static_cast<std::int16_t>(
-        GlyphMetrics.gmptGlyphOrigin.x);
-    dsc_out->ofs_y = static_cast<std::int16_t>(
-        GlyphMetrics.gmptGlyphOrigin.y - GlyphMetrics.gmBlackBoxY);
-    dsc_out->bpp = 8;
+    std::memcpy(
+        dsc_out,
+        &iterator->second.first,
+        sizeof(lv_font_glyph_dsc_t));
 
     return true;
 }
@@ -201,18 +213,7 @@ static const uint8_t* LvglWindowsGdiFontGetGlyphBitmapCallback(
         reinterpret_cast<PLVGL_WINDOWS_GDI_FONT_CONTEXT>(
             const_cast<void*>(font->dsc));
 
-    std::uint32_t DpiValue = LV_DPI_DEF;
-    lv_disp_t* CurrentDisplay = ::lv_disp_get_default();
-    if (CurrentDisplay)
-    {
-        DpiValue = CurrentDisplay->driver->dpi;
-    }
-
-    if (DpiValue != Context->DpiValue)
-    {
-        Context->GlyphSet.clear();
-        Context->DpiValue = DpiValue;
-    }
+    ::LvglWindowsGdiFontCheckDpiValue(Context);
 
     auto iterator = Context->GlyphSet.find(unicode_letter);
     if (iterator == Context->GlyphSet.end())
@@ -228,7 +229,11 @@ static const uint8_t* LvglWindowsGdiFontGetGlyphBitmapCallback(
         }
     }
 
-    return iterator->second.second;
+    std::size_t& GlyphBitmapOffset = iterator->second.second;
+    return
+        (GlyphBitmapOffset != static_cast<std::size_t>(-1))
+        ? &Context->GlyphBitmapPool[GlyphBitmapOffset]
+        : nullptr;
 }
 
 EXTERN_C void WINAPI LvglWindowsGdiFontInitialize(
@@ -325,26 +330,27 @@ EXTERN_C BOOL WINAPI LvglWindowsGdiFontCreateFont(
         {
             ::SelectObject(ContextDCHandle, Context->FontHandle);
 
+            OUTLINETEXTMETRICW OutlineTextMetrics;
             if (::GetOutlineTextMetricsW(
                 ContextDCHandle,
                 sizeof(OUTLINETEXTMETRICW),
-                &Context->OutlineTextMetrics))
+                &OutlineTextMetrics))
             {
                 FontObject->get_glyph_dsc =
                     ::LvglWindowsGdiFontGetGlyphCallback;
                 FontObject->get_glyph_bitmap =
                     ::LvglWindowsGdiFontGetGlyphBitmapCallback;
                 FontObject->line_height = static_cast<lv_coord_t>(
-                    Context->OutlineTextMetrics.otmLineGap
-                    - Context->OutlineTextMetrics.otmDescent
-                    + Context->OutlineTextMetrics.otmAscent);
+                    OutlineTextMetrics.otmLineGap
+                    - OutlineTextMetrics.otmDescent
+                    + OutlineTextMetrics.otmAscent);
                 FontObject->base_line = static_cast<lv_coord_t>(
-                    0 - Context->OutlineTextMetrics.otmDescent);
+                    0 - OutlineTextMetrics.otmDescent);
                 FontObject->subpx = LV_FONT_SUBPX_NONE;
                 FontObject->underline_position = static_cast<std::int8_t>(
-                    Context->OutlineTextMetrics.otmsUnderscorePosition);
+                    OutlineTextMetrics.otmsUnderscorePosition);
                 FontObject->underline_thickness = static_cast<std::int8_t>(
-                    Context->OutlineTextMetrics.otmsUnderscoreSize);
+                    OutlineTextMetrics.otmsUnderscoreSize);
 
                 IsSucceed = true;
             }
