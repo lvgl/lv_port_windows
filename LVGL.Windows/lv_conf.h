b07d0b0ba544040a5b3341ab9be5b69efccea9d3
MouriNaruto <Mouri_Naruto@Outlook.com>
2022-03-10 15:54:06 +0800

    Restore the default font setting for LVGL configuration.

--- LVGL.Windows/lv_conf.h
+++ LVGL.Windows/lv_conf.h
@@ -337,11 +337,10 @@
 /*Optionally declare custom fonts here.
  *You can use these fonts as default font too and they will be available globally.
  *E.g. #define LV_FONT_CUSTOM_DECLARE   LV_FONT_DECLARE(my_font_1) LV_FONT_DECLARE(my_font_2)*/
-#define LV_FONT_CUSTOM_DECLARE \
-    extern lv_font_t LvglDefaultFont;
+#define LV_FONT_CUSTOM_DECLARE
 
-/*Always set a default font*/
-#define LV_FONT_DEFAULT &LvglDefaultFont
+ /*Always set a default font*/
+#define LV_FONT_DEFAULT &lv_font_montserrat_14
 
 /*Enable handling large font and/or fonts with a lot of characters.
  *The limit depends on the font size, font face and bpp.
